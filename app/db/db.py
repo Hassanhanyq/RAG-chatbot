1b766d4fc55a10b4d093ce7535c4f174edcfd998
beaverman3426 <hoibeaver@gmail.com>
2025-06-22 20:04:04 +0300

    Update db.py
    attempted fix at the ssl problem

--- app/db/db.py
+++ app/db/db.py
@@ -7,7 +7,9 @@ from sqlalchemy.orm import sessionmaker
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-engine = create_async_engine(DATABASE_URL, echo=True)
+engine = create_async_engine(DATABASE_URL, echo=True, connect_args={
+    "ssl": True
+})
 
 AsyncSessionLocal = sessionmaker(
     bind=engine,
